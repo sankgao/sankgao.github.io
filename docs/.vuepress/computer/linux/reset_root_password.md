42312cccad11b98abfbf9cd27680238c893623da
gqingshan <root@localhost.localdomain>
2023-04-14 09:33:31 +0800

    Update file

---
+++ docs/.vuepress/computer/linux/reset_root_password.md
@@ -0,0 +1,58 @@
+---
+title: 重置 root 密码
+icon: passwd
+date: 2023-4-12
+category: Computer
+tag:
+  - Linux
+---
+
+## Linux 系统重置 root 密码
+
+1. 重启 Linux 系统主机并出现引导界面时，按下键盘上的 “e” 键进入内核编辑界面
+
+    ```bash
+    Red Hat Enterprise Linux (4.18.0-80.el8.x86_64) 8.0 (Ootpa)
+    Red Hat Enterprise Linux (0-rescue-2307b5f70ab0473fa42eea2fe914eb95) 8.0
+
+
+
+
+    User the ↑ and ↓ keys to change the selection.
+    Press 'e' to edit the selected item, or 'c' for a command prompt.
+    ```
+
+2. 在 Linux 参数这行的最后面追加 “rd.break” 参数，然后按下 “Ctrl + x” 组合键来运行修改过的内核程序
+
+    ```bash
+    load_video
+    set gfx_payload=keep
+    insmod gzio
+    linux ($root)/vmlinuz-4.18.0-80.el8.x86_64 root=/dev/mapper/rhel-root ro resum\
+    e=/dev/mapper/rhel-swap rd.lvm.lv=rhel/root rd.lvm.lv=rhel/swap rd.break
+    initrd ($root)/initramfs-4.18.0-80.el8.x86_64.img $tuned_initrd
+
+
+
+
+    Press Ctrl-x to start, Ctrl-c for a command prompt or Escape to
+    discard edits and return to the menu. Pressing Tab lists 
+    possible completions.
+    ```
+
+3. 大约 30 秒后，进入到系统的紧急求援模式
+
+    ```bash
+    switch_root:/# 
+    ```
+
+4. 依次输入以下命令，再连续按下两次 “Ctrl + d” 组合键来退出并重启，等待系统再次重启完毕后便可以使用新密码来登录 Linux 系统
+
+    ```bash
+    switch_root:/# mount -o remount,rw /sysroot
+    switch_root:/# chroot /sysroot
+    sh-4.4# passwd
+    sh-4.4# touch /.autorelabel
+    sh-4.4# exit
+    switch_root:/# logout
+    ```
--- docs/.vuepress/sidebar/zh.ts
+++ docs/.vuepress/sidebar/zh.ts
@@ -2,7 +2,7 @@ import { sidebar } from "vuepress-theme-hope";
 
 export const zhSidebarConfig = sidebar({
   // fallback
-  "/": ["", "note/", "code/", "software/", "about/"],
+  // "/": ["", "note/", "code/", "software/", "about/"],
   
   // 侧边栏配置
   "/computer/": "structure",
@@ -10,45 +10,45 @@ export const zhSidebarConfig = sidebar({
   "/computer/windows/": "structure",
   "/computer/linux/": "structure",
   
-  "/operations/": "structure",
-  "/operations/virtualization/": "structure",
-  "/operations/automation/": "structure",
-  "/operations/devops/": "structure",
-  "/operations/devops/git/": "structure",
-  "/operations/devops/jenkins/": "structure",
-  "/operations/devops/docker/": "structure",
-  "/operations/web/": "structure",
-  "/operations/monitor/": "structure",
-  "/operations/database/": "structure",
-  "/operations/wireshark/": "structure",
-  "/operations/object_oriented/": "structure",
-  "/operations/middleware/": "structure",
-  "/operations/message_queue/": "structure",
-  "/operations/cloud_computing/": "structure",
-  
-  "/framework/": "structure",
-  
-  "/language/": "structure",
-  "/language/shell/": "structure",
-  "/language/python/": "structure",
-  "/language/groovy/": "structure",
-  "/language/go/": "structure",
-  "/language/c/": "structure",
-  "/language/java/": "structure",
-  "/language/html/": "structure",
-  "/language/css/": "structure",
-  "/language/javascript/": "structure",
-  "/language/maven/": "structure",
-  
-  "/security/": "structure",
-  
-  "/other/": "structure",
-  
-  "/index/": "structure",
-  "/index/sites/": "structure",
-  "/index/sites/category/": "structure",
-  "/index/sites/tag/": "structure",
-  "/index/sites/timeline/": "structure",
-  "/index/my/": "structure",
-  "/index/my/about/": "structure",
+  // "/operations/": "structure",
+  // "/operations/virtualization/": "structure",
+  // "/operations/automation/": "structure",
+  // "/operations/devops/": "structure",
+  // "/operations/devops/git/": "structure",
+  // "/operations/devops/jenkins/": "structure",
+  // "/operations/devops/docker/": "structure",
+  // "/operations/web/": "structure",
+  // "/operations/monitor/": "structure",
+  // "/operations/database/": "structure",
+  // "/operations/wireshark/": "structure",
+  // "/operations/object_oriented/": "structure",
+  // "/operations/middleware/": "structure",
+  // "/operations/message_queue/": "structure",
+  // "/operations/cloud_computing/": "structure",
+  // 
+  // "/framework/": "structure",
+  // 
+  // "/language/": "structure",
+  // "/language/shell/": "structure",
+  // "/language/python/": "structure",
+  // "/language/groovy/": "structure",
+  // "/language/go/": "structure",
+  // "/language/c/": "structure",
+  // "/language/java/": "structure",
+  // "/language/html/": "structure",
+  // "/language/css/": "structure",
+  // "/language/javascript/": "structure",
+  // "/language/maven/": "structure",
+  // 
+  // "/security/": "structure",
+  // 
+  // "/other/": "structure",
+  // 
+  // "/index/": "structure",
+  // "/index/sites/": "structure",
+  // "/index/sites/category/": "structure",
+  // "/index/sites/tag/": "structure",
+  // "/index/sites/timeline/": "structure",
+  // "/index/my/": "structure",
+  // "/index/my/about/": "structure",
 });
