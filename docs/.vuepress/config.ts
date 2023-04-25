8d1b40f43dfff7612dcf33a93e63f5f67b5b79c2
Sankgao <sankgao@163.com>
2023-04-25 16:33:43 +0800

    添加搜索插件配置

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,4 +1,5 @@
 import { defineUserConfig } from "vuepress";
+import { docsearchPlugin } from "@vuepress/plugin-docsearch";
 import { dynamicTitlePlugin } from "@vuepress-denaro/vuepress-plugin-dynamic-title";
 
 import theme from "./theme.ts";
@@ -17,13 +18,13 @@ export default defineUserConfig({
   locales: {
     "/": {
       lang: "zh-CN",  // 站点的语言
-      title: "Sankgao Blog",  // 站点的标题
+      title: "Sankgao",  // 站点的标题
       description: "使用 VuePress-v2 搭建的个人博客",  // 站点的描述
     },
     
     "/en/": {
       lang: "en-US",
-      title: "Sankgao Blog",
+      title: "Sankgao",
       description: "A personal blog built using VuePress v2",
     },
   },
@@ -41,10 +42,61 @@ export default defineUserConfig({
   
   // 插件配置
   plugins: [
+    // DocSearch 搜索
+    docsearchPlugin({
+      appId: "0KZFY2PX1A",
+      apiKey: "26299b05be586e142ecf257f6a6ea1f1",
+      indexName: "sankgaoio",
+      locales: {
+        "/": {
+          placeholder: "搜索",
+          translations: {
+            button: {
+              buttonText: "搜索",
+              buttonAriaLabel: "搜索",
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: "清除查询条件",
+                resetButtonAriaLabel: "清除查询条件",
+                cancelButtonText: "取消",
+                cancelButtonAriaLabel: "取消",
+              },
+              startScreen: {
+                recentSearchesTitle: "搜索历史",
+                noRecentSearchesText: "没有搜索历史",
+                saveRecentSearchButtonTitle: "保存至搜索历史",
+                removeRecentSearchButtonTitle: "从搜索历史中移除",
+                favoriteSearchesTitle: "收藏",
+                removeFavoriteSearchButtonTitle: "从收藏中移除",
+              },
+              errorScreen: {
+                titleText: "无法获取结果",
+                helpText: "你可能需要检查你的网络连接",
+              },
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+                closeText: "关闭",
+                searchByText: "搜索提供者",
+              },
+              noResultsScreen: {
+                noResultsText: "无法找到相关结果",
+                suggestedQueryText: "你可以尝试查询",
+                reportMissingResultsText: "你认为该查询应该有结果？",
+                reportMissingResultsLinkText: "点击反馈",
+              },
+            },
+          },
+        },
+      },
+    }),
+    
+    // 动态标题
     dynamicTitlePlugin({
       showIcon: "/favicon.ico",
       showText: "ヾ(≧▽≦*)o欢迎回来！",
-      hideIcon: "/failure.ico",
+      hideIcon: "/favicon.ico",
       hideText: "ಥ_ಥ不要走呀！",
       recoverTime: 2000,
     }),
