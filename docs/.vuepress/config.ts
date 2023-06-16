3df09c1695f6bebd4ba91c50cb569a9fd1d5a9dd
Sankgao <sankgao@163.com>
2023-06-16 17:47:48 +0800

    添加鼠标点击和看板娘插件

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,6 +1,9 @@
 import { defineUserConfig } from "vuepress";
 import { docsearchPlugin } from "@vuepress/plugin-docsearch";
 import { dynamicTitlePlugin } from "@vuepress-denaro/vuepress-plugin-dynamic-title";
+import { PopperShape } from "@moefy-canvas/theme-popper";
+import { popperPlugin } from "./plugins/vuepress-plugin-popper";
+import { ohmylive2dPlugin } from 'vuepress-plugin-oh-my-live2d';
 
 import theme from "./theme.ts";
 
@@ -99,6 +102,84 @@ export default defineUserConfig({
       hideIcon: "/favicon.ico",
       hideText: "ಥ_ಥ不要走呀！",
       recoverTime: 2000,
+    }),
+
+    // 鼠标点击
+    popperPlugin({
+      config: {
+        shape: PopperShape.Star,
+        size: 1.95,
+        numParticles: 10,
+      },
+    }),
+
+    // 看板娘
+    ohmylive2dPlugin({
+      source: "/assets/live2d/vscode-live2d-models",
+      models: [
+        {
+          path: "/girls-frontline/G11/normal/model.json", scale: 0.4,
+        },
+        {
+          path: "/kesshouban/model.json", scale: 1.5, x: 70,
+        },
+        {
+          path: "/wanko/wanko.model.json", scale: 1.5, y: 130,
+        },
+        {
+          path: "/bilibili-22/index.json", scale: 2, x: 10,
+        },
+        {
+          path: "/potion-Maker-Pio/index.json", scale: 3.3,
+        },
+        {
+          path: "/haruto/haruto.model.json", scale: 1.5,
+        },
+        {
+          path: "/koharu/koharu.model.json", scale: 1.5,
+        },
+        {
+          path: "/hijiki/hijiki.model.json", scale: 1.5,
+        },
+        {
+          path: "/tororo/tororo.model.json", scale: 1.5,
+        },
+        {
+          path: "/kanzaki/kanzaki.model.json", scale: 2,
+          stageStyle: {
+            height: "330",
+          },
+        },
+        {
+          path: "/girls-frontline/AN94-2/normal/model.json", scale: 0.7,
+        },
+        {
+          path: "/girls-frontline/Kar98k/destroy/model.json", scale: 0.7,
+        },
+        {
+          path: "/girls-frontline/M4A1-2/destroy/model.json", scale: 0.5,
+        },
+        {
+          path: "/girls-frontline/M4-SOPMOD-II-2/destroy/model.json", scale: 0.7,
+        },
+        {
+          path: "/girls-frontline/WA2000-3/normal/model.json", scale: 0.7,
+        },
+        {
+          path: "/girls-frontline/WA2000-3/destroy/model.json", scale: 0.7,
+        },
+        {
+          path: "/girls-frontline/HK416-1/destroy/model.json", scale: 0.7, x: 10,
+        },
+      ],
+      tips: {
+        style: {
+          width: 150,
+          height: 70,
+          offsetX: 0,
+          offsetY: 70,
+        },
+      },
     }),
   ],
 })
