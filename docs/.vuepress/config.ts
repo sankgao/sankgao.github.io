05c55e952209f547817781f38a9da874b8ee4820
Sankgao <sankgao@163.com>
2023-07-12 18:08:48 +0800

    删除误操作配置

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,14 +1,11 @@
 import { defineUserConfig } from "vuepress";
 import { docsearchPlugin } from "@vuepress/plugin-docsearch";
-import { getDirname, path } from "@vuepress/utils";
 import { dynamicTitlePlugin } from "@vuepress-denaro/vuepress-plugin-dynamic-title";
 import { PopperShape } from "@moefy-canvas/theme-popper";
 import { popperPlugin } from "./plugins/vuepress-plugin-popper";
 import { ohmylive2dPlugin } from "vuepress-plugin-oh-my-live2d";
 
 import theme from "./theme.ts";
-
-const __dirname = getDirname(import.meta.url);
 
 export default defineUserConfig({
   // 默认站点配置
@@ -33,13 +30,6 @@ export default defineUserConfig({
       title: "Sank's blog",
       description: "A personal blog built using VuePress v2",
     },
-  },
-
-  alias: {
-    "@theme-hope/modules/blog/components/BlogHero": path.resolve(
-      __dirname,
-      "./components/BlogHero.vue"
-    ),
   },
   
   theme,  // 主题配置
