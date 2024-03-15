0bd48ba52125d730b44c4c6e5d96b9db4d7cde2f
Sankgao <sankgao@163.com>
2024-03-15 13:54:26 +0800

    :memo: 更新布局

--- docs/.vuepress/sidebar/computers/computers.ts
+++ docs/.vuepress/sidebar/computers/computers.ts
@@ -2,40 +2,28 @@ import { arraySidebar } from "vuepress-theme-hope";
 
 export const computers = arraySidebar([
   {
-    text: "基础",
+    text: "计算机基础",
     icon: "base",
     link: "base/",
     prefix: "base/",
-    children: [
-      "README",
-    ],
   },
   {
     text: "Windows",
     icon: "windows",
     link: "windows/",
     prefix: "windows/",
-    children: [
-      "README",
-    ],
   },
   {
     text: "Linux",
     icon: "linux",
     link: "linux/",
     prefix: "linux/",
-    children: [
-      "README",
-    ],
   },
   {
     text: "开发环境",
     icon: "backstage",
     link: "dev_env/",
     prefix: "dev_env/",
-    children: [
-      "README",
-    ],
   },
   {
     text: "架构",
