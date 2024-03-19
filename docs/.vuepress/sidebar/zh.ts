bcfd47bc991834f74c37492cb4cd3aa69f0a9bd1
Sankgao <Sankgao@163.com>
2024-03-19 20:46:25 +0800

    :memo: 添加 SVN 和 Gerrit 侧边栏

--- docs/.vuepress/sidebar/zh.ts
+++ docs/.vuepress/sidebar/zh.ts
@@ -1,6 +1,6 @@
 import { sidebar } from "vuepress-theme-hope";
 import { computers, framework } from "./computers/index.ts";
-import { devops } from "./devops/index.ts";
+import { devops, svn, gerrit } from "./devops/index.ts";
 import { languages, html5, css3, python3, c } from "./languages/index.ts";
 import { others } from "./others/index.ts";
 import { about } from "./about.ts";
@@ -14,6 +14,8 @@ export const zhSidebarConfig = sidebar({
   "/computers/framework/": framework,
   
   "/devops/": devops,
+  "/devops/svn/": svn,
+  "/devops/gerrit": gerrit,
   
   "/languages/": languages,
   "/languages/html5/": html5,
