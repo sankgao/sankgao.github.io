e9a8a7bb26c9dbbca31f8f98eefd36568b39dd04
sankgao <sankgao@163.com>
2024-03-26 10:58:50 +0000

    Deploying to gh-pages from @ sankgao/sankgao.github.io@e213012956bee2adf466e5e623221ba7f36bbcfd 🚀

---
+++ assets/comments.html-COnxpY_8.js
@@ -0,0 +1,10 @@
+import{_ as t}from"./plugin-vue_export-helper-x3n3nnut.js";import{r as a,o,c as l,d as c,w as i,f as r,b as n,e}from"./app-evoEPwaC.js";const d={},m=r("<p>注释标签 <code>&lt;!-- 与 --&gt;</code> 用于在 HTML 插入注释。</p><ul><li>注释标签在开始标签中有一个 <strong>惊叹号</strong>，但是结束标签中 <strong>没有</strong> 惊叹号</li><li>浏览器不会显示注释，但是能够帮助记录您的 HTML 文档</li><li>您可以利用注释在 HTML 中放置 <strong>通知</strong> 和 <strong>提醒信息</strong></li><li>可以注释一行文本，也可以注释多行文本</li></ul><p>例如：</p>",3),u=n("div",{class:"language-html line-numbers-mode","data-ext":"html"},[n("pre",{class:"language-html"},[n("code",null,[n("span",{class:"token comment"},"<!-- 这是一段注释 -->"),e(`
+
+`),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"<"),e("p")]),n("span",{class:"token punctuation"},">")]),e("这是一个段落。"),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"</"),e("p")]),n("span",{class:"token punctuation"},">")]),e(`
+
+`),n("span",{class:"token comment"},`<!-- 
+  记得在
+  此处
+  添加信息
+-->`),e(`
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1);function p(g,_){const s=a("CodeDemo");return o(),l("div",null,[m,c(s,{id:"code-demo-28",type:"normal",title:"%E6%BC%94%E7%A4%BA",code:"eJyrVsooyc1RslKyUdTVVXixf+azGeuf7Gh4tm7rs80rXrZ3Kejq2sXkxeTZFNjBJZ/sWAWUfzFh7+OGJhv9Aog8SHtMnoLCi3Ubnu6b/nTOChDn2dolT5e0gFnbdz/tWvBk/8Jnjetj8sCGKtUCAKTAQDg="},{default:i(()=>[u]),_:1})])}const h=t(d,[["render",p],["__file","comments.html.vue"]]);export{h as default};
