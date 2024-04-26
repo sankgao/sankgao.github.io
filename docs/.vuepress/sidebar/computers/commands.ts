7e53a2ee7d637c7b871a5e1546623b37e253c6c8
Sankgao <sankgao@163.com>
2024-04-26 18:11:38 +0800

    :memo: 添加命令集导航栏和侧边栏

---
+++ docs/.vuepress/sidebar/computers/commands.ts
@@ -0,0 +1,31 @@
+import { arraySidebar } from "vuepress-theme-hope";
+
+export const commands = arraySidebar([
+  {
+    text: "Linux",
+    icon: "linux",
+    link: "linux/",
+    prefix: "linux/",
+    collapsible: true,
+    children: [
+    ],
+  },
+  {
+    text: "Git",
+    icon: "git",
+    link: "git/",
+    prefix: "git/",
+    collapsible: true,
+    children: [
+    ],
+  },
+  {
+    text: "Docker",
+    icon: "docker",
+    link: "docker/",
+    prefix: "docker/",
+    collapsible: true,
+    children: [
+    ],
+  },
+]);
