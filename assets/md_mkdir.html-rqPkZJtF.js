766b841dc012f7be892954297d2590e6b72a6dec
sankgao <sankgao@163.com>
2024-07-25 09:57:56 +0000

    Deploying to gh-pages from @ sankgao/sankgao.github.io@31af98e91d5caec234ecafa6ba5a2b6f73f993a4 🚀

---
+++ assets/md_mkdir.html-rqPkZJtF.js
@@ -0,0 +1,31 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as d,c as i,f as n}from"./app-PVD15qjF.js";const s={},a=n(`<h2 id="描述" tabindex="-1"><a class="header-anchor" href="#描述" aria-hidden="true">#</a> 描述</h2><p><code>md</code> 或 <code>mkdir</code> 用于创建目录或子目录。命令扩展（默认情况下启用）允许使用单个 <code>md</code> 命令在指定路径中创建中间目录。</p><h2 id="选项" tabindex="-1"><a class="header-anchor" href="#选项" aria-hidden="true">#</a> 选项</h2><table><thead><tr><th style="text-align:center;">选项</th><th style="text-align:left;">描述</th></tr></thead><tbody><tr><td style="text-align:center;"><code>/?</code></td><td style="text-align:left;">在命令提示符下显示帮助</td></tr></tbody></table><h2 id="示例" tabindex="-1"><a class="header-anchor" href="#示例" aria-hidden="true">#</a> 示例</h2><h3 id="创建目录" tabindex="-1"><a class="header-anchor" href="#创建目录" aria-hidden="true">#</a> 创建目录</h3><p>例如：在当前目录中创建 <code>test04</code> 目录。</p><p>创建前先查看当前目录：</p><div class="language-cmd line-numbers-mode" data-ext="cmd"><pre class="language-cmd"><code>E:\\test&gt;dir /b
+hello.txt
+test 03
+test01
+test02
+
+E:\\test&gt;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>创建 <code>test04</code> 目录：</p><div class="language-cmd line-numbers-mode" data-ext="cmd"><pre class="language-cmd"><code>E:\\test&gt;md test04
+
+E:\\test&gt;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>创建后查看当前目录：</p><div class="language-cmd line-numbers-mode" data-ext="cmd"><pre class="language-cmd"><code>E:\\test&gt;dir /b
+hello.txt
+test 03
+test01
+test02
+test04
+
+E:\\test&gt;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="创建递归目录" tabindex="-1"><a class="header-anchor" href="#创建递归目录" aria-hidden="true">#</a> 创建递归目录</h3><p>例如：在当前目录中递归创建 <code>a\\b\\c</code> 目录。</p><div class="language-cmd line-numbers-mode" data-ext="cmd"><pre class="language-cmd"><code>E:\\test&gt;md a\\b\\c
+
+E:\\test&gt;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>创建后查看当前目录：</p><div class="language-cmd line-numbers-mode" data-ext="cmd"><pre class="language-cmd"><code>E:\\test&gt;dir /b
+a
+hello.txt
+test 03
+test01
+test02
+test04
+
+E:\\test&gt;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,18),t=[a];function l(c,r){return d(),i("div",null,t)}const u=e(s,[["render",l],["__file","md_mkdir.html.vue"]]);export{u as default};
