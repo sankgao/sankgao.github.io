b467b914096b63648e06dbd784018a951441fc79
sankgao <sankgao@163.com>
2024-11-18 09:08:53 +0000

    Deploying to gh-pages from @ sankgao/sankgao.github.io@f18385876b8be2b71b4d61babddade14f0726908 🚀

---
+++ assets/exit.html-vR_IhAod.js
@@ -0,0 +1,8 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as t,c as d,f as a}from"./app-oliGifly.js";const i={},n=a(`<h2 id="描述" tabindex="-1"><a class="header-anchor" href="#描述" aria-hidden="true">#</a> 描述</h2><p><code>exit</code> 用于退出命令解释器或当前的批处理脚本。</p><h2 id="选项" tabindex="-1"><a class="header-anchor" href="#选项" aria-hidden="true">#</a> 选项</h2><table><thead><tr><th style="text-align:center;">选项</th><th style="text-align:left;">描述</th></tr></thead><tbody><tr><td style="text-align:center;"><code>/b</code></td><td style="text-align:left;">退出当前的批处理脚本，而不是退出 <code>Cmd.exe</code>。如果从批处理脚本外部执行，则退出 <code>Cmd.exe</code></td></tr><tr><td style="text-align:center;"><code>/?</code></td><td style="text-align:left;">在命令提示符下显示帮助</td></tr></tbody></table><h2 id="示例" tabindex="-1"><a class="header-anchor" href="#示例" aria-hidden="true">#</a> 示例</h2><p>例如：退出当前命令解释器。</p><div class="language-cmd line-numbers-mode" data-ext="cmd"><pre class="language-cmd"><code>E:\\test&gt;cmd
+Microsoft Windows [版本 10.0.19045.4651]
+(c) Microsoft Corporation。保留所有权利。
+
+E:\\test&gt;exit
+
+E:\\test&gt;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,7),c=[n];function r(s,l){return t(),d("div",null,c)}const m=e(i,[["render",r],["__file","exit.html.vue"]]);export{m as default};
